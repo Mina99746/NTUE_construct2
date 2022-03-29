91d37bef47e721f088fcc795b5b5ad1cf70823d8
110944013 <77623851+Mina99746@users.noreply.github.com>
2022-03-29 15:20:10 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1648538267,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/hole-sheet0.png",
+		"images/rabbit-sheet0.png",
+		"images/rabbit-sheet1.png",
+		"images/hammer-sheet0.png",
+		"media/mixkit-animal-eating-herb-2241.m4a",
+		"media/mixkit-animal-eating-herb-2241.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
